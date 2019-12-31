ee00220804e5de50023c45e5bff8009397553906
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-12-30 20:51:53 -0500

    trying to connect to database

---
+++ mysql.js
@@ -0,0 +1,43 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "3317",
+	database: "animecsv"
+});
+
+let query = "";
+
+db.connect((err) => {
+	if (err) throw err;
+	console.log("You have successfully connected!");
+	query = "DROP TABLE IF EXISTS ANIME";
+	db.query(query, (err) => {
+		if (err) throw err;
+		console.log("Anime table has been dropped!");
+	});
+
+	query =
+		"CREATE TABLE ANIME (row_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), " +
+		"anime_id INT, type VARCHAR(255), episodes INT, rating DECIMAL, members INT)";
+
+	db.query(query, (err) => {
+		if (err) throw err;
+		console.log("Your Anime table has been created!");
+	});
+
+	(query = "LOAD DATA LOCAL INFILE 'csv/animeTable.csv' INTO TABLE ANIME FIELDS TERMINATED BY "),
+		"IGNORE 1 LINES" + "(name, anime_id, type, episodes, rating, members)";
+
+	db.query(query, (err) => {
+		if (err) throw err;
+		console.log("Your Anime table has been loaded!");
+	});
+
+	db.end((err) => {
+		if (err) throw err;
+		console.log("It's all good! The database connection is closing");
+	});
+});
